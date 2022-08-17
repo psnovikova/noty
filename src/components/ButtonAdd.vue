d3ca4e69c9cf6602e4bc23b3ea0b062faf9ccdc6
psnovikova <nps20052000@mail.ru>
2022-08-17 11:43:47 +0300

    Component without extra lines

--- src/components/ButtonAdd.vue
+++ src/components/ButtonAdd.vue
@@ -3,13 +3,9 @@ import { PlusIcon } from '@heroicons/vue/outline'
 </script>
 
 <template>
-  <button class="p-3 bg-stone-600 rounded-2xl hover:bg-stone-700 fixed bottom-24 sm:bottom-6 right-6">
-    <span>
-      <PlusIcon class="h-8" />
-    </span>
+  <button class="p-3 bg-stone-600 rounded-2xl hover:bg-stone-700 ">
+    <PlusIcon class="h-8" />
   </button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped />
