252d576d60d4329cd020e89a872d4635dbb449f7
psnovikova <nps20052000@mail.ru>
2022-08-23 15:31:30 +0300

    оптимизирован код

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,7 +1,5 @@
 <script setup>
-import router from '../router'
-
-const props = defineProps({
+defineProps({
   note: {
     type: Object,
     required: true,
@@ -10,28 +8,26 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="w-1/2">
-    <RouterLink
-      :to="`/notes/${note.id}`"
-    >
-      <div class="h-20 bg-emerald-400 m-3 rounded-xl p-4 text-black">
-        <h1 v-if="note.title">
-          {{ note.title }}
-        </h1>
-        <h1 v-else-if="note.text && !note.title">
-          {{ note.text }}
-        </h1>
-        <h1 v-else class="text-stone-500">
-          пустая заметка
-        </h1>
-        <p class="text-stone-600 mt-1">
-          {{ new Date(note.updatedAt).toLocaleDateString() }}
-        </p>
-      </div>
-    </RouterLink>
-  </div>
+  <RouterLink :to="`/notes/${note.id}`">
+    <div class="m-2 rounded-xl p-4 text-black" :style="{ backgroundColor: note.color }">
+      <h1 v-if="note.title">
+        {{ truncate(note.title) }}
+      </h1>
+      <h1 v-else-if="note.text">
+        {{ truncate(note.text) }}
+      </h1>
+      <h1 v-else class="text-stone-500">
+        пустая заметка
+      </h1>
+      <p class="text-stone-600 mt-1">
+        {{ new Date(note.updatedAt).toLocaleDateString() }}
+      </p>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
-
+h1 {
+  @apply break-words
+}
 </style>
